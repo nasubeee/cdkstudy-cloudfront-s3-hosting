7cdfd908564457fde409737055f4d99de0752621
Yoshiyuki Yamariku <55145573+nasubifujii@users.noreply.github.com>
2021-04-13 09:02:17 +0900

    feat: Add Website distribution stack.

---
+++ bin/app.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { WebDistributionStack } from '../lib/webdistribution';
+import { ResourceName } from '../lib/resource_name';
+
+const app = new cdk.App();
+
+// Get Context
+const systemName = app.node.tryGetContext("system_name");
+const systemEnv = app.node.tryGetContext("env");
+const resourceName = new ResourceName(systemName, systemEnv);
+
+// Define stack env
+const stackEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION
+};
+
+// Create web distribution stack
+new WebDistributionStack(app, '01-web-dist', {
+  env: stackEnv,
+  resourceName: resourceName,
+});
+
+// Create waf stack to us-east-1 region
+
